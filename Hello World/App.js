96b45b075ca8dce46b34f63dd44329c27ed40a5e
kristine-harris <72718929+kristine-harris@users.noreply.github.com>
2023-04-10 00:58:19 +0000

    Submission for PE1

---
+++ Hello World/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Kristine Harris</Text>
+      <Text>MSCSI</Text>
+      <Text>STC</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFF00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
